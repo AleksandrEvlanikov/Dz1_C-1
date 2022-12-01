14338299eda3062ae609ed815d7c2b505af21d14
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-01 16:38:55 +0300

    Dz4

---
+++ Dz4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(" Введите число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = - number1;
+if (number1 % 2 == 0 );
+else
+{
+    Console.WriteLine(number1);
+    number2 = number2 + 1;
+}
+
+
+while (number2 <= number1)
+{
+    Console.WriteLine(number2);
+    number2 = number2 + 2;
+}
