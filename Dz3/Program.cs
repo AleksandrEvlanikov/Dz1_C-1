8f9210fa69f9d955a4402f0db2ec7b09a9051bf0
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-01 16:38:02 +0300

    Dz3

---
+++ Dz3/Program.cs
@@ -0,0 +1,11 @@
+﻿int a;
+Console.WriteLine(" введите число " );
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 1 )
+{ 
+    Console.WriteLine(" число нечетное");
+}
+else
+{
+    Console.WriteLine("число четное");
+}
